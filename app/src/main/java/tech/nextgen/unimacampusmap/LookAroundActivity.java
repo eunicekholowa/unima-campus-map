23a0ac626ed7b77822b2b877a18af1299cac5f04
KhamaPride <bsc-40-16@cc.ac.mw>
2022-02-16 04:13:20 -0700

    Added location indicator and add feature services to the main activity

---
+++ app/src/main/java/tech/nextgen/unimacampusmap/LookAroundActivity.java
@@ -0,0 +1,14 @@
+package tech.nextgen.unimacampusmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LookAroundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_look_around);
+    }
+}
