070dc9e2cfff72c6681dde640f6d2009937b1e30
KhamaPride <bsc-40-16@cc.ac.mw>
2022-04-01 03:58:06 -0700

    Added location indicator and add feature services to the main activity

---
+++ app/src/main/java/tech/nextgen/unimacampusmap/MapActivity.java
@@ -0,0 +1,359 @@
+package tech.nextgen.unimacampusmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+import static android.content.ContentValues.TAG;
+import static android.os.Build.VERSION_CODES.S;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+import android.widget.Toast;
+
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.data.FeatureEditResult;
+import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.layers.FeatureLayer;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.BasemapStyle;
+import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import tech.nextgen.unimacampusmap.spinner.ItemData;
+import tech.nextgen.unimacampusmap.spinner.SpinnerAdapter;
+
+public class MapActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+    private LocationDisplay mLocationDisplay;
+    private Spinner mSpinner;
+
+    private final int requestCode = 2;
+    private final String[] reqPermissions = { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission
+            .ACCESS_COARSE_LOCATION };
+
+    private ServiceFeatureTable mServiceFeatureTable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        // authentication with an API key or named user is required to access basemaps and other
+        // location services
+        ArcGISRuntimeEnvironment.setApiKey("AAPKae2e0e4cb2814af49d6346db0343b8f9NyzRssRkbMgKM_Oc-amCwMPW__5G4lDx9CTq4dP3T7vXSpLv1uM22fr7t50GNn-a");
+
+        // Get the Spinner from layout
+        mSpinner = findViewById(R.id.spinner);
+
+        // Get the MapView from layout and set a map with the BasemapType Imagery
+        mMapView = findViewById(R.id.mapView);
+        ArcGISMap map = new ArcGISMap(BasemapStyle.OSM_STANDARD);
+
+        //set the map to be displayed in this view
+        mMapView.setMap(map);
+        mMapView.setViewpoint(new Viewpoint( -15.3897, 35.3370,7000));
+
+
+        // get the MapView's LocationDisplay
+        mLocationDisplay = mMapView.getLocationDisplay();
+
+        // Listen to changes in the status of the location data source.
+        mLocationDisplay.addDataSourceStatusChangedListener(dataSourceStatusChangedEvent -> {
+
+            // If LocationDisplay started OK, then continue.
+            if (dataSourceStatusChangedEvent.isStarted())
+                return;
+
+            // No error is reported, then continue.
+            if (dataSourceStatusChangedEvent.getError() == null)
+                return;
+
+            // If an error is found, handle the failure to start.
+            // Check permissions to see if failure may be due to lack of permissions.
+            boolean permissionCheck1 = ContextCompat.checkSelfPermission(this, reqPermissions[0]) ==
+                    PackageManager.PERMISSION_GRANTED;
+            boolean permissionCheck2 = ContextCompat.checkSelfPermission(this, reqPermissions[1]) ==
+                    PackageManager.PERMISSION_GRANTED;
+
+            if (!(permissionCheck1 && permissionCheck2)) {
+                // If permissions are not already granted, request permission from the user.
+                ActivityCompat.requestPermissions(this, reqPermissions, requestCode);
+            } else {
+                // Report other unknown failure types to the user - for example, location services may not
+                // be enabled on the device.
+                String message = String.format("Error in DataSourceStatusChangedListener: %s", dataSourceStatusChangedEvent
+                        .getSource().getLocationDataSource().getError().getMessage());
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+                // Update UI to reflect that the location display did not actually start
+                mSpinner.setSelection(0, true);
+            }
+        });
+
+        // Populate the list for the Location display options for the spinner's Adapter
+        ArrayList<ItemData> list = new ArrayList<>();
+        list.add(new ItemData("Stop", R.drawable.locationdisplaydisabled));
+        list.add(new ItemData("On", R.drawable.locationdisplayon));
+        list.add(new ItemData("Re-Center", R.drawable.locationdisplayrecenter));
+        list.add(new ItemData("Navigation", R.drawable.locationdisplaynavigation));
+        list.add(new ItemData("Compass", R.drawable.locationdisplayheading));
+
+        SpinnerAdapter adapter = new SpinnerAdapter(this, R.layout.spinner_layout, R.id.txt, list);
+        mSpinner.setAdapter(adapter);
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 0:
+                        // Stop Location Display
+                        if (mLocationDisplay.isStarted())
+                            mLocationDisplay.stop();
+                        break;
+                    case 1:
+                        // Start Location Display
+                        if (!mLocationDisplay.isStarted())
+                            mLocationDisplay.startAsync();
+                        break;
+                    case 2:
+                        // Re-Center MapView on Location
+                        // AutoPanMode - Default: In this mode, the MapView attempts to keep the location symbol on-screen by
+                        // re-centering the location symbol when the symbol moves outside a "wander extent". The location symbol
+                        // may move freely within the wander extent, but as soon as the symbol exits the wander extent, the MapView
+                        // re-centers the map on the symbol.
+                        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.RECENTER);
+                        if (!mLocationDisplay.isStarted())
+                            mLocationDisplay.startAsync();
+                        break;
+                    case 3:
+                        // Start Navigation Mode
+                        // This mode is best suited for in-vehicle navigation.
+                        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.NAVIGATION);
+                        if (!mLocationDisplay.isStarted())
+                            mLocationDisplay.startAsync();
+                        break;
+                    case 4:
+                        // Start Compass Mode
+                        // This mode is better suited for waypoint navigation when the user is walking.
+                        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
+                        if (!mLocationDisplay.isStarted())
+                            mLocationDisplay.startAsync();
+                        break;
+                }
+
+            }
+
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+
+        });
+        mServiceFeatureTable = new ServiceFeatureTable(getString(R.string.service_layer_url));
+
+        // create a feature layer from table
+        FeatureLayer featureLayer = new FeatureLayer(mServiceFeatureTable);
+
+        // add the layer to the map
+        map.getOperationalLayers().add(featureLayer);
+
+        // add a listener to the MapView to detect when a user has performed a single tap to add a new feature to
+        // the service feature table
+        mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mMapView) {
+            @Override public boolean onSingleTapConfirmed(MotionEvent event) {
+                // create a point from where the user clicked
+                android.graphics.Point point = new android.graphics.Point((int) event.getX(), (int) event.getY());
+
+                // create a map point from a point
+                Point mapPoint = mMapView.screenToLocation(point);
+
+                // add a new feature to the service feature table
+                addFeature(mapPoint, mServiceFeatureTable);
+                return super.onSingleTapConfirmed(event);
+            }
+        });
+
+    }
+
+    /**
+     * Adds a new Feature to a ServiceFeatureTable and applies the changes to the
+     * server.
+     *
+     * @param mapPoint     location to add feature
+     * @param featureTable service feature table to add feature
+     */
+    private void addFeature(Point mapPoint, final ServiceFeatureTable featureTable) {
+
+        // create default attributes for the feature
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("typdamage", "Destroyed");
+        attributes.put("primcause", "Earthquake");
+
+        // creates a new feature using default attributes and point
+        Feature feature = featureTable.createFeature(attributes, mapPoint);
+
+        // check if feature can be added to feature table
+        if (featureTable.canAdd()) {
+            // add the new feature to the feature table and to server
+            featureTable.addFeatureAsync(feature).addDoneListener(() -> applyEdits(featureTable));
+        } else {
+            runOnUiThread(() -> logToUser(true, getString(R.string.error_cannot_add_to_feature_table)));
+        }
+    }
+
+    /**
+     * Sends any edits on the ServiceFeatureTable to the server.
+     *
+     * @param featureTable service feature table
+     */
+    @SuppressLint("StringFormatInvalid")
+    private void applyEdits(ServiceFeatureTable featureTable) {
+
+        // apply the changes to the server
+        final ListenableFuture<List<FeatureEditResult>> editResult = featureTable.applyEditsAsync();
+        editResult.addDoneListener(() -> {
+            try {
+                List<FeatureEditResult> editResults = null;
+                try {
+                    editResults = editResult.get();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+                // check if the server edit was successful
+                if (editResults != null && !editResults.isEmpty()) {
+                    if (!editResults.get(0).hasCompletedWithErrors()) {
+                        runOnUiThread(() -> logToUser(false, getString(R.string.feature_added)));
+                    } else {
+                        throw editResults.get(0).getError();
+                    }
+                }
+            } catch (InterruptedException e) {
+                runOnUiThread(() -> logToUser(true, getString(R.string.error_applying_edits, e.getCause().getMessage())));
+            }
+        });
+    }
+
+    /**
+     * Shows a Toast to user and logs to logcat.
+     *
+     * @param isError whether message is an error. Determines log level.
+     * @param message message to display
+     */
+    private void logToUser(boolean isError, String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        if (isError) {
+            Log.e(TAG, message);
+        } else {
+            Log.d(TAG, message);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // If request is cancelled, the result arrays are empty.
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Location permission was granted. This would have been triggered in response to failing to start the
+            // LocationDisplay, so try starting this again.
+            mLocationDisplay.startAsync();
+        } else {
+            // If permission was denied, show toast to inform user what was chosen. If LocationDisplay is started again,
+            // request permission UX will be shown again, option should be shown to allow never showing the UX again.
+            // Alternative would be to disable functionality so request is not shown again.
+            Toast.makeText(this, getString(R.string.location_permission_denied), Toast.LENGTH_SHORT).show();
+
+            // Update UI to reflect that the location display did not actually start
+            mSpinner.setSelection(0, true);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.dispose();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Handle item selection
+        switch (item.getItemId()) {
+            case R.id.drawer:
+                //perform any action;
+                return true;
+            //open main app page
+            case R.id.mapMenu:
+                Intent mapIntent = new Intent(this,MainActivity.class);
+                startActivity(mapIntent);
+
+            case R.id.myPlacesMenu:
+                Intent myPlacesIntent = new Intent(this,MyPlaces.class);
+                startActivity(myPlacesIntent);
+
+            case R.id.lookAroundMenu:
+                Intent lookAroundIntent = new Intent(this, LookAroundActivity.class);
+            case R.id.arNavMenu:
+                //perform any action;
+                return true;
+            case R.id.settingsMenu:
+                //perform any action;
+                return true;
+            case R.id.helpMenu:
+                //perform any action;
+                return true;
+            case R.id.sendFdbkMenu:
+                //perform any action;
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
